bdaeec070b1b982fe7db27a2e0795a85c7750ca7
Andrew Ellender <ajellender@gmail.com>
2022-11-21 12:41:51 +0000

    feat: implement simple XHR routes for classes, locations and trainers indeces

---
+++ app/Http/Controllers/Api/ClassController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ClassListResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Yadda\Enso\Facades\EnsoCrud;
+
+class ClassController extends Controller
+{
+    const DEFAULT_PER_PAGE = 12;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        if (Gate::denies('viewAny', EnsoCrud::modelClass('class'))) {
+            abort(404);
+        }
+
+        $query = $this->getIndexQuery($request);
+
+        return ClassListResource::collection(
+            $query->paginate($request->get('per_page', static::DEFAULT_PER_PAGE))
+        )->toResponse($request);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        abort(404);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        abort(404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        abort(404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        abort(404);
+    }
+
+    /**
+     * Generate an index query based on the given request
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function getIndexQuery(Request $request): \Illuminate\Database\Eloquent\Builder
+    {
+        $query = EnsoCrud::query('class');
+
+        $query->when($request->filled('location'), function  ($query) use ($request) {
+            $query->whereHas('locations', function ($query) use ($request) {
+                $query->where('slug', $request->get('location'));
+            });
+        });
+
+        $query->when($request->filled('trainer'), function  ($query) use ($request) {
+            $query->whereHas('trainers', function ($query) use ($request) {
+                $query->where('slug', $request->get('trainer'));
+            });
+        });
+
+        return $query;
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,8 +44,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
+                ->name('api.')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware(['web', 'enso'])
