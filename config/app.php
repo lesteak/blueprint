db2344e704cd31bebfeb51e37222fc37314fab6f
Andrew Ellender <ajellender@gmail.com>
2022-11-21 15:18:56 +0000

    feat: implement Locations ViewComposer

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EnsoServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ViewServiceProvider::class,
 
     ],
 
