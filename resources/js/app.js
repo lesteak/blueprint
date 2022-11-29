0d1f3667cc134d47c44985acc90ec699830c340c
Joshua Jones <josh@jtbj.co.uk>
2022-11-29 20:55:48 +0000

    BMA-44 feat: add general resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,20 @@ import EnsoCarousel from 'enso-carousel';
 import MainMenu from './enso/MainMenu.js';
 import BlogIndex from './enso/blog/components/BlogIndex.vue';
 
+import MoreLinks from './components/more/More.vue';
+import ArticleIndex from './components/articles/ArticleIndex.vue';
+import ClassesIndex from './components/classes/ClassesIndex.vue';
+import LocationIndex from './components/locations/LocationIndex.vue'
+
 new Vue({
   el: '#app',
   components: {
     BlogIndex,
     EnsoCarousel,
     MainMenu,
+    MoreLinks,
+    ArticleIndex,
+    ClassesIndex,
+    LocationIndex
   },
 });
