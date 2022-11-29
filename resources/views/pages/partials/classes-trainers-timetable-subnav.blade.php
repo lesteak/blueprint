b3bd48be2822f0150834e770ed02c3328205a39f
Joshua Jones <josh@jtbj.co.uk>
2022-11-29 20:56:33 +0000

    BMA-110 feat: sub nav component

--- resources/views/pages/partials/classes-trainers-timetable-subnav.blade.php
+++ resources/views/pages/partials/classes-trainers-timetable-subnav.blade.php
@@ -1,1 +1,57 @@
-<p>Implement Subnav here</p>
+@php
+$menu = SiteMenu::get('sub-menu');
+@endphp
+
+<section>
+  <div
+    class="
+      w-full
+      bg-gradient-to-r
+      from-[#25272e]
+      to-brand-grey-500
+      h-16
+      flex
+      justify-center
+    "
+  >
+    <ul
+      class="
+        flex
+        md:justify-center
+        justify-between
+        items-center
+        h-full
+        md:w-2/3
+        lg:w-1/2
+        xl:w-1/3
+        w-full
+      "
+    >
+      @foreach ($menu->items as $item)
+      <li
+        class="
+          border-r
+          border-black
+          w-full
+          h-full
+          {{ Nav::isCurrent($item->url) ? 'bg-white text-black md:rounded-tl-lg md:rounded-tr-lg' : 'text-white' }}
+        "
+      >
+        <a
+          href="{{ $item->url }}"
+          target="{{ $item->target_str }}"
+          rel="{{ $item->rel }}"
+          class="
+            h-full
+            items-center
+            flex
+            justify-center
+          "
+        >
+          {{ $item->label }}
+        </a>
+      </li>
+      @endforeach
+    </ul>
+  </div>
+</section>
