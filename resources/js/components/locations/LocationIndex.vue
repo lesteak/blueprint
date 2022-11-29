c8fa0111a313dcc91ef995f1ebef96118428ffc8
Joshua Jones <josh@jtbj.co.uk>
2022-11-29 20:58:49 +0000

    BMA-44 chore: add missing vue components

---
+++ resources/js/components/locations/LocationIndex.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+      v-if="locations"
+      class="
+        grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-3
+        grid
+        gap-5
+      "
+    >
+      <div
+        v-for="location in locations"
+        :key="location.id"
+      >
+        <card :data="location"></card>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Api from "../../Services/api";
+  import Card from "../global/InfoCard.vue";
+  export default {
+    components: {
+      Card,
+    },
+    props: {
+      classType: {
+        required: false
+      },
+      trainer: {
+        required: false
+      }
+    },
+    data () {
+      return {
+        locations: null
+      }
+    },
+    mounted() {
+      this.load();
+    },
+    methods: {
+      load() {
+        Api.get("/api/locations")
+          .then(res => {
+            this.locations = res.data.data
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      }
+    }
+  }
+  </script>
