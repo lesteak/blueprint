e88893f3507b7e3a76362c95e0946f8800545580
Joshua Jones <josh@jtbj.co.uk>
2022-11-30 15:48:32 +0000

    BMA-44 chore: split up footer components

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -4,56 +4,11 @@ $footer_more = \Yadda\Enso\SiteMenus\Facades\SiteMenu::get('footer-menu');
 
 $menus = new \Illuminate\Support\Collection([$footer_locations, $footer_more]);
 
-$button = collect(EnsoSettings::get('location_footer_button'));
-//$image = collect(EnsoSettings::get('location_footer_image')[0]);
-//dd($image);
-
 @endphp
 
 <footer>
-  <section class="max-w-screen-2xl m-auto p-10 my-20 flex w-full flex-col">
-    <div
-      class="
-        grid
-        grid-cols-1
-        md:grid-cols-3
-        bg-gradient-to-r
-        from-[#25272e]
-        to-brand-grey-500
-        text-white
-      "
-    >
-      <div
-        class="
-          col-span-2
-          flex
-          flex-col
-          justify-center
-          gap-10
-          md:p-20
-          p-10
-          order-2
-          md:order-1
-        "
-      >
-        <h2>{{ EnsoSettings::get('location_footer_title') }}</h2>
-        {{ EnsoSettings::get('location_footer_content') }}
-        @if (EnsoSettings::get('location_footer_button'))
-          {{--  <x-button-group :buttons="$button" class="mt-8"></x-button-group>  --}}
-        @endif
-      </div>
-      {{--  <img
-        class="
-          order-1
-          md:order-2
-          h-full
-          object-cover
-          w-full
-        "
-        src="{{ $image['preview'] }}"
-        alt="Our Locations">  --}}
-    </div>
-  </section>
+
+  @include('parts.locations-footer');
 
   <div
     class="
@@ -88,15 +43,18 @@ $button = collect(EnsoSettings::get('location_footer_button'));
             </ul>
           @endif
         @endforeach
-        <ul class="border-t md:border-l md:border-t-0 border-brand-grey-500 pt-4 md:pl-4">
-          <li class="text-white text-4xl font-teko">Social</li>
-          @if (EnsoSettings::get('instagram_url'))
-            <li><a href="{{ EnsoSettings::get('instagram_url') }}">instagram</a></li>
-          @endif
-          @if (EnsoSettings::get('facebook_url'))
-            <li><a href="{{ EnsoSettings::get('facebook_url') }}">facebook</a></li>
-          @endif
-        </ul>
+
+        @if (EnsoSettings::get('instagram_url') || EnsoSettings::get('facebook_url'))
+          <ul class="border-t md:border-l md:border-t-0 border-brand-grey-500 pt-4 md:pl-4">
+            <li class="text-white text-4xl font-teko">Social</li>
+            @if (EnsoSettings::get('instagram_url'))
+              <li><a href="{{ EnsoSettings::get('instagram_url') }}">instagram</a></li>
+            @endif
+            @if (EnsoSettings::get('facebook_url'))
+              <li><a href="{{ EnsoSettings::get('facebook_url') }}">facebook</a></li>
+            @endif
+          </ul>
+        @endif
       </nav>
     </section>
 
