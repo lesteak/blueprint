a922d4fbd852cfbec3204c45519f2eee66a552b3
Joshua Jones <josh@jtbj.co.uk>
2023-01-10 12:39:18 +0000

    BMA-44 chore: add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,15 @@
     @include('enso-meta::display')
     <link href="https://fonts.googleapis.com/css2?family=Miriam+Libre:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f8c848">
+    <meta name="msapplication-TileColor" content="#3d363d">
+    <meta name="theme-color" content="#3d363d">
+
   </head>
   <body class="flex">
     @include('enso-analytics::body')
--- resources/views/trainers/show.blade.php
+++ resources/views/trainers/show.blade.php
@@ -1,3 +1,30 @@
+@php
+    $cards = [];
+    if (EnsoSettings::get('more_links_trainers_url'))
+    {
+        $cards[] = [
+            "name" => EnsoSettings::get('more_links_trainers_title'),
+            "img" => collect(EnsoSettings::get('more_links_trainers_image'))->first(),
+            "url" => EnsoSettings::get('more_links_trainers_url')
+        ];
+    }
+    if (EnsoSettings::get('more_links_classes_url'))
+    {
+        $cards[] = [
+            "name" => EnsoSettings::get('more_links_classes_title'),
+            "img" => collect(EnsoSettings::get('more_links_classes_image'))->first(),
+            "url" => EnsoSettings::get('more_links_classes_url')
+        ];
+    }
+    if (EnsoSettings::get('more_links_timetable_url'))
+    {
+        $cards[] = [
+            "name" => EnsoSettings::get('more_links_timetable_title'),
+            "img" => collect(EnsoSettings::get('more_links_timetable_image'))->first(),
+            "url" => EnsoSettings::get('more_links_timetable_url')
+        ];
+    }
+@endphp
 @extends('layouts.app')
 
 @section('content')
@@ -91,6 +118,6 @@
 
   <section class="max-w-screen-2xl m-auto p-10">
     <h2 class="text-5xl md:text-8xl">More</h2>
-    <more-links :hide="'/classes'"></more-links>
+    <more-links :cards='@json($cards)' :hide="'/classes'"></more-links>
   </section>
 @endsection
